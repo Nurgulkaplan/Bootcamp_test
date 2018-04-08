00b22a5f24c1671845b30ffcc266f565c98403b2
Nurgul Kaplan <nurgul.kaplan@icloud.com>
2018-04-07 21:20:11 -0700

    SocialAnalytics_20180407_assignment

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "g1IagxtsWzOf5WrOoPbBMAE5j"
+consumer_secret = "GBEUyavo9GmtUHhoLQgaZabhssOEogtq1tio7dVzK630GE9Zdn"
+access_token = "980968880255393794-HzZ7crQX0Jaqr4rvbTR80vbwgYwhfvw"
+access_token_secret = "fFMy8NxTbHKtcXM9FGe138WNqio0yWu3bzYmOVCXc3hb6"
